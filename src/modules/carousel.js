b2eb6648f2b5de9156e8713dcfce4db0d5d8164f
IvoLinux <ivolin0706@gmail.com>
2023-05-11 16:33:44 -0300

    Carrosel update

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -6,13 +6,15 @@ export default function Garbage() {
    return <div className="carousel-inner">
       <Carousel>
          <Carousel.Item>
-            <Link href="http://www.ipcfex.eb.mil.br/ultimas-noticias/313-19-simposio-internacional-de-atividade-fisica-3"><img alt="SIAFIS" src="images/carrossel1.png" /></Link>
+            <Link href="https://pt.calameo.com/read/006435344567665a09633?authid=e15gBQsiCu7E">
+               <img alt="Informativo AsEFEx" src="images/carrossel0.png" />
+            </Link>
             <div className="galleria-info">
                <div className="galleria-info-text">
                   <div className="galleria-info-title">
-                     <h3><Link href="http://www.ipcfex.eb.mil.br/ultimas-noticias/313-19-simposio-internacional-de-atividade-fisica-3">SIAFIS</Link></h3>
+                     <h3><Link href="https://pt.calameo.com/read/006435344567665a09633?authid=e15gBQsiCu7E">Informativo AsEFEx</Link></h3>
                   </div>
-                  <div className="galleria-info-description">Confira o conteúdo do XIX Simpósio Internacional de Atividade Física.</div>
+                  <div className="galleria-info-description">Confira as matérias da Edição 117 nossa revista</div>
                   {/* <div data-index={0} style={{ display: 'block' }} className="rights">
                      Nome do autor da imagem</div> */}
                </div>
@@ -20,15 +22,15 @@ export default function Garbage() {
          </Carousel.Item>
 
          <Carousel.Item>
-            <Link href="http://www.esefex.eb.mil.br/ultimas-noticias/280-vii-forum-cientifico">
-               <img alt="VIII Fórum Científico" src="images/carrossel2.png" />
+            <Link href="https://asefex.com.br/diretoria-executiva">
+               <img alt="Diretoria Executiva AsEFEx" src="images/carrossel1.png" />
             </Link>
             <div className="galleria-info">
                <div className="galleria-info-text">
                   <div className="galleria-info-title">
-                     <h3><Link href="http://www.esefex.eb.mil.br/ultimas-noticias/280-vii-forum-cientifico">VIII FÓRUM CIENTÍFICO</Link></h3>
+                     <h3><Link href="https://asefex.com.br/diretoria-executiva">Diretoria Executiva AsEFEx</Link></h3>
                   </div>
-                  <div className="galleria-info-description">Confira as datas e o tema do evento.</div>
+                  <div className="galleria-info-description">Equipe para o biênio 2022 - 2023.</div>
                   {/* <div data-index={0} style={{ display: 'block' }} className="rights">
                      Nome do autor da imagem</div> */}
                </div>
@@ -36,15 +38,15 @@ export default function Garbage() {
          </Carousel.Item>
 
          <Carousel.Item>
-            <Link href="https://pt.calameo.com/read/0064353440d972455e25a?authid=7B9aI7IM9Xtj">
-               <img alt="Informativo AsEFEx" src="images/carrossel3.png" />
+            <Link href="http://www.esefex.eb.mil.br/ultimas-noticias/294-esefex-100-anos">
+               <img alt="Encontro Nacional dos Calções Pretos 2022" src="images/carrossel2.png" />
             </Link>
             <div className="galleria-info">
                <div className="galleria-info-text">
                   <div className="galleria-info-title">
-                     <h3><Link href="https://pt.calameo.com/read/0064353440d972455e25a?authid=7B9aI7IM9Xtj">Informativo AsEFEx</Link></h3>
+                     <h3><Link href="http://www.esefex.eb.mil.br/ultimas-noticias/294-esefex-100-anos">Encontro Nacional dos Calções Pretos 2022</Link></h3>
                   </div>
-                  <div className="galleria-info-description">Acesse a última edição do nosso Informativo.</div>
+                  <div className="galleria-info-description">Venha celebrar conosco o centenário da EsEFEx - 17 DEZ 22.</div>
                   {/* <div data-index={0} style={{ display: 'block' }} className="rights">
                      Nome do autor da imagem</div> */}
                </div>
@@ -52,15 +54,31 @@ export default function Garbage() {
          </Carousel.Item>
 
          <Carousel.Item>
-            <Link href="https://pt.calameo.com/read/006435344d3a13a215c69?authid=G7WRiQYJICNf">
-               <img alt="Revista AsEFEx" src="images/carrossel4.png" />
+            <Link href="https://pt.calameo.com/read/006435344e3ff145ebcc5?authid=IcwOmLTWn2AD">
+               <img alt="Revista AsEFEx 115" src="images/carrossel3.png" />
             </Link>
             <div className="galleria-info">
                <div className="galleria-info-text">
                   <div className="galleria-info-title">
-                     <h3><Link href="https://pt.calameo.com/read/006435344d3a13a215c69?authid=G7WRiQYJICNf">Revista AsEFEx</Link></h3>
+                     <h3><Link href="https://pt.calameo.com/read/006435344e3ff145ebcc5?authid=IcwOmLTWn2AD">Informativo AsEFEx</Link></h3>
                   </div>
-                  <div className="galleria-info-description">Leia a última edição de nossa Revista.</div>
+                  <div className="galleria-info-description">Confira as matérias da Edição 116 nossa revista.</div>
+                  {/* <div data-index={0} style={{ display: 'block' }} className="rights">
+                     Nome do autor da imagem</div> */}
+               </div>
+            </div>
+         </Carousel.Item>
+
+         <Carousel.Item>
+            <Link href="https://pt.calameo.com/read/006435344f848ad6cf41a?authid=s9AUDEJk0ONc">
+               <img alt="Informativo AsEFEx" src="images/carrossel4.png" />
+            </Link>
+            <div className="galleria-info">
+               <div className="galleria-info-text">
+                  <div className="galleria-info-title">
+                     <h3><Link href="https://pt.calameo.com/read/006435344f848ad6cf41a?authid=s9AUDEJk0ONc">Informativo AsEFEx</Link></h3>
+                  </div>
+                  <div className="galleria-info-description">Confira as matérias da Edição 115 nossa revista.</div>
                   {/* <div data-index={0} style={{ display: 'block' }} className="rights">
                      Nome do autor da imagem</div> */}
                </div>
