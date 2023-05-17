f481ade08a8b73085e0fddc12ac8af5851c16fe6
IvoLinux <ivolin0706@gmail.com>
2023-05-17 07:52:49 -0300

    Fix API path

--- src/pages/api/galeria-ano/[ano].js
+++ src/pages/api/galeria-ano/[ano].js
@@ -2,6 +2,6 @@ import {promises} from 'fs'
 
 export default async function handler(req, res){
     const { ano } = req.query
-    const jsonContents = await promises.readFile('./assets/TabelasCalcoesPretos/' + ano + '.json', 'utf8');
+    const jsonContents = await promises.readFile(process.cwd() + '/assets/TabelasCalcoesPretos/' + ano + '.json', 'utf8');
     res.status(200).json(jsonContents);
 }
